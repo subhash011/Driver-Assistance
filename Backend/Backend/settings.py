cb16bb9f162801b34e6fa5b6c5580c64dad32fb0
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2021-04-12 20:08:40 +0530

    authorization added

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 REST_USE_JWT = True
